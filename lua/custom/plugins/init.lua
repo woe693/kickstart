4bcbc1f0d6aeac66f10f3033c55de481b1c9dbca
woe <woe6933@gmail.com>
2024-04-22 16:20:52 -1200

    customizing some stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'NStefan002/screenkey.nvim',
+  cmd = 'Screenkey',
+  version = '*',
+  config = true,
+}
---
+++ lua/woe/init.lua
@@ -0,0 +1,10 @@
+require 'woe.remap'
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+
+-- Save undo history
+vim.opt.undofile = true
